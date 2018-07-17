a9e25d1f10fead44db9e057a00183399400b39af
Donanas <37550444+Donanas@users.noreply.github.com>
2018-07-17 23:26:04 +0800

    Ao

--- City_Color/City_MainColor.py
+++ City_Color/City_MainColor.py
@@ -4,7 +4,9 @@ Created on Tue Jul 17 16:59:16 2018
 
 @author: Ao
 """
-
+#print(_doc_)
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
 from numpy.random import rand
 from scipy import misc
@@ -21,12 +23,14 @@ import json
 
 
 '''
-***part A
+***
+part A
 以文件夹名为键，值包含文件夹下所有文件名的列表，文件类型自定义
 ***
 '''
+
 #dirpath="D:\python\Deng\poi_get\charts"
-#fileType=["jpg"]
+#fileType=[" "]
 #'''
 def filePath(dirpath,fileType):
     fileInfo={}
@@ -38,3 +42,97 @@ def filePath(dirpath,fileType):
             if tempList:
                 fileInfo.setdefault(dirpath,tempList)
     return fileInfo
+
+'''
+***
+part B
+读取图像rgb数组，并调整图像大小，减少计算时间，调整图像数组为2维，用于下一步的计算
+***
+'''
+
+def getPixData(img):
+    lum_img=mpimg.imread(img)
+    print(lum_img)
+    lum_imgSmall=misc.imresize(lum_img,0.3)
+    h,w,d=lum_imgSmall.shape
+    pixData=np.reshape(lum_imgSmall,(h*w, d))
+    return lum_imgSmall,pixData
+
+'''
+***
+part C
+聚类的方法提取图像主题色，并打印图像、聚类预测的二位显示和主题色
+***
+'''
+def cityColorThemes(imgInfo):
+    #设置聚类参数，实验中仅使用了Kmeans算法，其他算法执行尝试
+    default_base = {'quantile': .3,
+                    'eps': .3,
+                    'damping': 0.9,
+                    'preference': -200,
+                    'n_neighbors': 10,
+                    'n_cluster': 7}
+    datasets=[((1[1],None),()) for i in imgInfo]#基于pixData的图像数据，用于聚类计算
+    imgList=[i[0] for i in imgInfo]#基于lum_imgSmall的数据图像，用于图像显示，imgInfo为图像列表
+#   print(datasets[0])
+    #官方聚类案例中对于datasets的配置
+    
+    
+    themes=np.zeros((default_base['n_clusters'], 3))#建立0占位的数组，用于后面主题数据的追加。n_clusters是提取主题色的色彩聚类数量，此处为7，轴2为3，是RGB的数值
+    (img,pix)=imgInfo[0]#可以一次性提取元素索引相同的值，img就是lum_imgSmall，而pix是pixData
+    pixV,pixH=pix.shape
+    pred=np.zeros((pixV))
+    plt.figure(figsize=(6*3+3, len(imgInfo)*2))#设置图标大小，根据图象的数量来设置高度
+    plt.subplots_adjust(left= .02, right=.98, bottom=.001, top=.96, wspace=.3, hspace=.3)
+    plot_num=1
+    
+    for i_dataset, (dataset, algo_params) in enumerate(datasets):#循环pixData数据，即将预测的每个图像数据。enumerate()函数将可迭代队象组成一个索引序列，可以同时获取索引和值，其中i_dataset为索引，从0开始
+        print(i_dataset)
+        params=default_base.copy()
+        #print(params)
+        params.update(algo_params)
+        #print(params)
+        X, y = dataset#用于机器学习的数据一般包括特征数据和类标，此次实验为无监督分类的聚类，没有类标，并将其在前文中设置为None对象
+        #print(X.shape,X)
+        Xstd = StandardScalar().fit_transform(X)
+        
+        two_means = cluster.MiniBatchKMeans(n_clusters=params['n_clusters'])
+        
+        spectral = cluster.SpectralClustering(n_clusters=params['n_clusters'],
+                                              eigen_solver='arpack',affinity="nearest_neighbors")
+        dbscan = cluster.DBSCAN(eps=params['eps'])
+        dffinity_propagation = cluster.AffinityPropagation(damping=params['damping'],
+                                                           preference=params['preference'])
+        birch = cluster.Birch(n_clusters=params['n_clusters'])
+        qmm = mixture.GaussianMixture(n_components=params['n_cluster'],covariance_type='full')
+        km=cluster.KMeans(n_clusters=params['n_clusters'])
+        
+        
+        clustering_algorithms=(
+                ('KMeans',km),
+                )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ City_Color/字典练习.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 17 22:54:48 2018
+
+@author: Administrator
+"""
+
+A={'a':2,'b':3}
+for i in A:
+    print(i[0])
