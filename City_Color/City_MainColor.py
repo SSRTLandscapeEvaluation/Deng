6b00a0427ee8ba39b657e67da974bd3bbd4f7b70
Donanas <37550444+Donanas@users.noreply.github.com>
2018-07-18 19:27:54 +0800

    Ao

--- City_Color/City_MainColor.py
+++ City_Color/City_MainColor.py
@@ -52,8 +52,8 @@ part B
 
 def getPixData(img):
     lum_img=mpimg.imread(img)
-    print(lum_img)
-    lum_imgSmall=misc.imresize(lum_img,0.3)
+#    print(lum_img)
+    lum_imgSmall=misc.imresize(lum_img,0.1)
     h,w,d=lum_imgSmall.shape
     pixData=np.reshape(lum_imgSmall,(h*w, d))
     return lum_imgSmall,pixData
@@ -71,8 +71,8 @@ def cityColorThemes(imgInfo):
                     'damping': 0.9,
                     'preference': -200,
                     'n_neighbors': 10,
-                    'n_cluster': 7}
-    datasets=[((1[1],None),()) for i in imgInfo]#基于pixData的图像数据，用于聚类计算
+                    'n_clusters': 7}
+    datasets=[((i[1],None),{ }) for i in imgInfo]#基于pixData的图像数据，用于聚类计算
     imgList=[i[0] for i in imgInfo]#基于lum_imgSmall的数据图像，用于图像显示，imgInfo为图像列表
 #   print(datasets[0])
     #官方聚类案例中对于datasets的配置
@@ -87,7 +87,7 @@ def cityColorThemes(imgInfo):
     plot_num=1
     
     for i_dataset, (dataset, algo_params) in enumerate(datasets):#循环pixData数据，即将预测的每个图像数据。enumerate()函数将可迭代队象组成一个索引序列，可以同时获取索引和值，其中i_dataset为索引，从0开始
-        print(i_dataset)
+#        print(i_dataset)
         params=default_base.copy()
         #print(params)
         params.update(algo_params)
@@ -95,7 +95,10 @@ def cityColorThemes(imgInfo):
         X, y = dataset#用于机器学习的数据一般包括特征数据和类标，此次实验为无监督分类的聚类，没有类标，并将其在前文中设置为None对象
         #print(X.shape,X)
         Xstd = StandardScaler().fit_transform(X)
-        
+      
+        '''
+        ***参数设定ST***
+        '''
         two_means = cluster.MiniBatchKMeans(n_clusters=params['n_clusters'])
         
         spectral = cluster.SpectralClustering(n_clusters=params['n_clusters'],
@@ -104,20 +107,22 @@ def cityColorThemes(imgInfo):
         dffinity_propagation = cluster.AffinityPropagation(damping=params['damping'],
                                                            preference=params['preference'])
         birch = cluster.Birch(n_clusters=params['n_clusters'])
-        qmm = mixture.GaussianMixture(n_components=params['n_cluster'],covariance_type='full')
+        qmm = mixture.GaussianMixture(n_components=params['n_clusters'],covariance_type='full')
         km=cluster.KMeans(n_clusters=params['n_clusters'])
-        
+        '''
+        ***参数设定END***
+        '''
         
         clustering_algorithms=(
                 ('KMeans',km),
                 )
         for name, algorithm in clustering_algorithms:
             t0=time.time()
-            with warnings.catch_warning():
+            with warnings.catch_warnings():
                 warnings.filterwarnings(
                         "ignore",
-                        massage="the number of connected ,spectral embedding"+"may not work as expected",
-                        cateqory = UserWarning)
+                        message="the number of connected ,spectral embedding"+"may not work as expected",
+                        category = UserWarning)
                 algorithm.fit(X)
             quantize=np.array(algorithm.cluster_centers_,dtype=np.uint8)
             themes=np.vstack((themes,quantize))
@@ -126,7 +131,7 @@ def cityColorThemes(imgInfo):
             if hasattr(algorithm,'labels'):
                 y_pred = algorithm.labels_.astype(np.int)
             else:
-                y_pred = algorithm.prediot(X)
+                y_pred = algorithm.predict(X)
             
             pred=np.vstack((pred, y_pred))
             
@@ -136,7 +141,8 @@ def cityColorThemes(imgInfo):
                 plt.title(name, size=18)
             colors = np.array(list(islice(cycle(['#377eb8','#ff7f00','#4daf4a',
                                              '#f781bf','#a65628','#984ea3',
-                                             '#999999','#e41a1c','#dede00']),int(max(y_pred)+1))))
+                                             '#999999','#e41a1c','#dede00']),
+                                             int(max(y_pred)+1))))
             plt.scatter(Xstd[:, 0], Xstd[:, 1], s=10, color=colors[y_pred])
             plt.xlim(-2.5,2.5)
             plt.ylim(-2.5,2.5)
@@ -145,10 +151,81 @@ def cityColorThemes(imgInfo):
             plt.text(.99,.01,('%.2fs' % (t1 - t0)).lstrip('0'),
                  transform=plt.gca().transAxes,size=15,
                  horizontalalignment ='right')
-            
+            '''
+            配置主题色子图参数配置
+            '''
+            plt.subplot(len(datasets), figWidth, plot_num+2)
+            t=1
+            pale=np.zeros(imgList[i_dataset].shape, dtype=np.uint8)
+            h,w,_=pale.shape
+            ph=h/len(quantize)
+            for y in range(h):
+                pale[y,::] = np.array(quantize[int(y/ph)], dtype=np.uint8)
+            plt.imshow(pale)
+            t+=1
+            plot_num+=3
+    plt.show()
+    return themes,pred
 
+'''
+***
+part D
+显示所有图像主题色
+***
+'''
+def cityColorImpression(themes):
+    n_samples = themes.shape[0]
+#    print(n_samples)
+    random_state = 170
+    #利用scikit的datasets数据集构建有差异话的斑点
+    varied=datasets.make_blobs(n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5],random_state=random_state)
+#    print(varied)
+    (x,y)=varied
+    fig, ax=plt.subplots(figsize=(10,10))
+    scale=1000  #设置斑点随机大小
+    print(y)
+    print(themes)
+    ax.scatter(x[...,0], x[...,1], c=themes/255,s=scale,alpha=0.7, edgecolors='none')
+    ax.grid(True)
+    plt.show()
+    
 
+'''
+***
+part E
+保存文件。因为对于大量数据聚类计算花费时间较长，因此建议将数据储存在文件中，以备以后调用。savingData()函数将文件储存为json数据格式
+***
+'''
+def savingData(data,savingPath,name):
+    jsonFile=open(os.path.join(savingPath,str(time.time())+r'_cityColorImpression_%s.json'%name),'w')
+    json.dump(data.tolist(),jsonFile)
+    jsonFile.close()
 
+if __name__ == "__main__" :
+    savingPath=r"D:\python\Deng\City_Color\test"
+    dirpath=r"D:\python\Deng\City_Color\test"
+    fileType=["jpg"]
+    fileInfo=filePath(dirpath,fileType)
+#    print(fileInfo)
+    filePathKeys=list(fileInfo.keys())
+    selection=0
+    imgPath=filePathKeys[selection]
+#    print(imgPath)
+    imgList=fileInfo[filePathKeys[selection]]
+    imgPathList=[os.path.join(imgPath,i) for i in imgList]
+    imgInfo=[(getPixData(img)) for img in imgPathList]
+#    print(imgPathList)
+#    print(imgInfo[0][1])
+
+    themes,pred=cityColorThemes(imgInfo)
+    cityColorImpression(themes)
+    
+    nameThemes=r'themes'
+    savingData(themes,savingPath,nameThemes)
+    namePred=r'pred'
+    savingData(pred,savingPath,namePred)
+    
+    
 
 
 
