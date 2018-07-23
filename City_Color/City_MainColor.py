d44b93a50a95a0ae2f74e4f36b29da5af3b66d4f
Donanas <37550444+Donanas@users.noreply.github.com>
2018-07-23 11:37:25 +0800

    A

--- City_Color/City_MainColor.py
+++ City_Color/City_MainColor.py
@@ -71,64 +71,99 @@ def cityColorThemes(imgInfo):
                     'damping': 0.9,
                     'preference': -200,
                     'n_neighbors': 10,
-                    'n_clusters': 7}
+                    'n_clusters': 7} 
+    #定义参数字典
+
+
     datasets=[((i[1],None),{ }) for i in imgInfo]#基于pixData的图像数据，用于聚类计算
-    imgList=[i[0] for i in imgInfo]#基于lum_imgSmall的数据图像，用于图像显示，imgInfo为图像列表
-#   print(datasets[0])
+#    print(datasets)
+#   imgInfo=[(getPixData(img)) for img in imgPathList]
+    imgList=[i[0] for i in imgInfo]#基于lum_imgSmall的数据图像，用于图像显示，imgInfo为图像列表，
+#    print(imgList)
     #官方聚类案例中对于datasets的配置
     
     
-    themes=np.zeros((default_base['n_clusters'], 3))#建立0占位的数组，用于后面主题数据的追加。n_clusters是提取主题色的色彩聚类数量，此处为7，轴2为3，是RGB的数值
-    (img,pix)=imgInfo[0]#可以一次性提取元素索引相同的值，img就是lum_imgSmall，而pix是pixData
-    pixV,pixH=pix.shape
-    pred=np.zeros((pixV))
+    themes=np.zeros((default_base['n_clusters'], 3))#建立0占位的数组，用于后面主题数据的追加。n_clusters是提取主题色的色彩聚类数量，此处为7，轴2为3，(7,3)，是RGB的数值
+    (img,pix)=imgInfo[0]#可以一次性提取元素索引相同的值，img就是lum_imgSmall，而pix是pixData，lum_imgSmall是缩放后的三维数组，pixData是降维后的数组
+#    print(len(img))
+#    print('\n')
+#    print(pix.shape)
+#    print('\n')
+    pixV,pixH=pix.shape#V是一张图像里的所有像素点个数，H是每个像素对应了R，G，B三种值
+#    print(pixV)
+    pred=np.zeros((pixV))#0值占位，
     plt.figure(figsize=(6*3+3, len(imgInfo)*2))#设置图标大小，根据图象的数量来设置高度
     plt.subplots_adjust(left= .02, right=.98, bottom=.001, top=.96, wspace=.3, hspace=.3)
     plot_num=1
     
+#    print('*x*'+'\n')
+#    print(enumerate(datasets))
+#    print('*x**'+'\n')
     for i_dataset, (dataset, algo_params) in enumerate(datasets):#循环pixData数据，即将预测的每个图像数据。enumerate()函数将可迭代队象组成一个索引序列，可以同时获取索引和值，其中i_dataset为索引，从0开始
 #        print(i_dataset)
         params=default_base.copy()
         #print(params)
-        params.update(algo_params)
+        params.update(algo_params)#替换原来的，更新数据
         #print(params)
         X, y = dataset#用于机器学习的数据一般包括特征数据和类标，此次实验为无监督分类的聚类，没有类标，并将其在前文中设置为None对象
-        #print(X.shape,X)
-        Xstd = StandardScaler().fit_transform(X)
-      
+        
+        Xstd = StandardScaler().fit_transform(X)#只用于二维显示，不用于聚类计算
+ 
+#        print('***'+'\n')
+#        print(Xstd)
+#        print('***'+'\n')
         '''
-        ***参数设定ST***
+        ***参数设定Start***
         '''
         two_means = cluster.MiniBatchKMeans(n_clusters=params['n_clusters'])
         
         spectral = cluster.SpectralClustering(n_clusters=params['n_clusters'],
                                               eigen_solver='arpack',affinity="nearest_neighbors")
         dbscan = cluster.DBSCAN(eps=params['eps'])
-        dffinity_propagation = cluster.AffinityPropagation(damping=params['damping'],
+        affinity_propagation = cluster.AffinityPropagation(damping=params['damping'],
                                                            preference=params['preference'])
         birch = cluster.Birch(n_clusters=params['n_clusters'])
-        qmm = mixture.GaussianMixture(n_components=params['n_clusters'],covariance_type='full')
+        gmn = mixture.GaussianMixture(n_components=params['n_clusters'],covariance_type='full')
         km=cluster.KMeans(n_clusters=params['n_clusters'])
         '''
         ***参数设定END***
         '''
         
         clustering_algorithms=(
-                ('KMeans',km),
+#                ('Kmeans', km),
+                ('MiniBatchKMeans', two_means),
+#                ('AffinityPropagation', affinity_propagation),
+#                ('MeanShift', ms),
+#                ('SpectralClusterig', spectral),
+#                ('Ward', ward),
+#                ('AgglcmerativeClustering', average_linkage),
+#                ('DBSCAN',dbscan),
+#                ('birch',birch),
+#                ('GaussianMixture',gmn)
                 )
+#        print('***'+'\n')
+#        print(len(clustering_algorithms))
+#        print('***'+'\n')
+#循环算法
         for name, algorithm in clustering_algorithms:
+#            print('***'+'\n')
+#            print(name)
+#            print('***'+'\n')
+#            print(algorithm)
+#            print('***'+'\n')
             t0=time.time()
+#            print (time.asctime(time.localtime(time.time())))
             with warnings.catch_warnings():
                 warnings.filterwarnings(
                         "ignore",
                         message="the number of connected ,spectral embedding"+"may not work as expected",
                         category = UserWarning)
                 algorithm.fit(X)
-            quantize=np.array(algorithm.cluster_centers_,dtype=np.uint8)
-            themes=np.vstack((themes,quantize))
+            quantize=np.array(algorithm.cluster_centers_,dtype=np.uint8)#提取聚类的中心，定义为主题色
+            themes=np.vstack((themes,quantize))#数据追加主题色
             t1=time.time()
             
-            if hasattr(algorithm,'labels'):
+            if hasattr(algorithm, 'labels'):
                 y_pred = algorithm.labels_.astype(np.int)
             else:
                 y_pred = algorithm.predict(X)
@@ -136,6 +171,8 @@ def cityColorThemes(imgInfo):
             pred=np.vstack((pred, y_pred))
             
             figWidth = (len(clustering_algorithms)+2)*3
+            plt.subplot(len(datasets), figWidth, plot_num)
+            plt.imshow(imgList[i_dataset])
             plt.subplot(len(datasets), figWidth, plot_num+1)
             if i_dataset == 0:
                 plt.title(name, size=18)
