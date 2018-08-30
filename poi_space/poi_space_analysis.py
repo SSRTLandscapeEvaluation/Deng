7f2333a4b42626175c841db11aca20a1da819187
Donanas <37550444+Donanas@users.noreply.github.com>
2018-08-30 11:03:49 +0800

    ao

--- poi_space/poi_space_analysis.py
+++ poi_space/poi_space_analysis.py
@@ -17,7 +17,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.datasets import base
 from sklearn import cluster,covariance, manifold
 from scipy.stats import chi2_contingency
- 
+
 
 
 '''
@@ -42,3 +42,46 @@ flatten_lst=lambda lst: [m for n_lst in lst for m in flatten_lst(n_lst)] if type
 
 '''
 C提取分析所需数据，并转换为sklearn的bunch储存方法，统一格式，方便读取。注意poi行业分类类标
+'''
+
+def jsonDataFilter(fileInfo):
+    rootPath=list(fileInfo.keys())
+#    print(rootPath)
+    dataName=flatten_lst(list(fileInfo.values()))
+    coodiDic=[]
+    for fName in dataName:
+        f=open(os.path.join(rootPath[0],fName))
+        jsonDecodes=json.load(f)
+        coodiDic.append([(coordi['location']['lat'],coordi['location']['lng'],fName[:-5]) for coordi in jsonDecodes])
+        coodiDic=flatten_lst(coodiDic)
+#    print(coodiDic)
+    data=np.array([(v[0],v[1]) for v in coodiDic])
+    targetNames=np.array([v[2] for v in coodiDic])
+    print(data)
+    print(targetNames)
+    class_label=labeRncoder()
+    targetLabel=class_label.fit_transform(targetNames)
+    class_mapping=[(idx,label) for idx,label in enumerate(class_label.classes_)]#建立以及分类名和整理编码映射列表
+    #print(class_mapping)
+    dataBunch=base.Bunch(DESCR=r'spatial points datasets of poi',data=data,feature_mames=["XCoordinate","yCoordinate"],target=targetLabel,target_names=class_mapping)
+    return dataBunch,class_mapping
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
